de30c5ef3a2d4bd2d61b36469b9f2def8e92de1a
Anysofiah <Anyhdz23@unitec.edu>
2025-09-25 14:13:12 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,74 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'App Framework7',
+	panel: {
+		swipe: true,
+	},
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/tiendas/',
+    	url: 'tiendas.html',
+    	name: 'tiendas',
+  		},
+		{
+		path: '/todo/',
+    	url: 'todo.html',
+    	name: 'todo',
+  		},
+		{
+		path: '/acerca/',
+    	url: 'acerca.html',
+    	name: 'acerca',
+  		},
+		{
+		path: '/perfil/',
+    	url: 'perfil.html',
+    	name: 'perfil',
+  		},
+		{
+		path: '/product/',
+    	url: 'product.html',
+    	name: 'product',
+  		},
+		
+		
+	],
+	dialog: {
+		title: 'Diseño Web App',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+
+
